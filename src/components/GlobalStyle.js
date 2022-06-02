89b83b674e191a61012c0b232c365264e54737b2
Sergio Follyvi <62280198+serfoll@users.noreply.github.com>
2022-06-02 08:01:35 +0200

    style guide

--- src/components/GlobalStyle.js
+++ src/components/GlobalStyle.js
@@ -1,8 +1,49 @@
 import { createGlobalStyle } from 'styled-components';
 import normalize from 'normalize.css';
+import Colors from './Colors';
+
+//FONTS
+const manropeEot = new URL(
+  '../fonts/manrope-v12-latin-800.eot',
+  import.meta.url
+);
+const manropeEotIEFix = new URL(
+  '../fonts/manrope-v12-latin-800.eot?#iefix',
+  import.meta.url
+);
+const manropeWoff2 = new URL(
+  '../fonts/manrope-v12-latin-800.woff2',
+  import.meta.url
+);
+const manropeWoff = new URL(
+  '../fonts/manrope-v12-latin-800.woff',
+  import.meta.url
+);
+const manropeTtf = new URL(
+  '../fonts/manrope-v12-latin-800.ttf',
+  import.meta.url
+);
+const manropeSvg = new URL(
+  '../fonts/manrope-v12-latin-800.svg#Manrope',
+  import.meta.url
+);
 
 export default createGlobalStyle`
   ${normalize}
+
+  /* manrope-800 - latin */
+@font-face {
+  font-family: 'Manrope';
+  font-style: normal;
+  font-weight: 800;
+  src: url(${manropeEot}); /* IE9 Compat Modes */
+  src: local(''),
+       url(${manropeEotIEFix}) format('embedded-opentype'), /* IE6-IE8 */
+       url(${manropeWoff2}) format('woff2'), /* Super Modern Browsers */
+       url(${manropeWoff}) format('woff'), /* Modern Browsers */
+       url(${manropeTtf}) format('truetype'), /* Safari, Android, iOS */
+       url(${manropeSvg}) format('svg'); /* Legacy iOS */
+}
 
   *,
   *:before,
@@ -15,8 +56,8 @@ export default createGlobalStyle`
   }
 
   body{
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
+    font-family: 'Manrope',-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
       Oxygen-Sans, Ubuntu, Cantarell, 'Helvetica Neue', sans-serif;
-    background-color: #263238;
+    background-color: ${Colors['dark-blue']};
   }
 `;
